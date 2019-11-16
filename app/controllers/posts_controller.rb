861d3756661598347749e1cfcc81b98c769209c1
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-16 17:30:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,11 @@ class PostsController < ApplicationController
 
   def show
   @post = Post.find(params[:id])
-  respond_to do |format|
-    format.html { render :show }
-    format.json { render json: @post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])} 
-  end
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])} 
+    end
+end 
 
   def new
     @post = Post.new
